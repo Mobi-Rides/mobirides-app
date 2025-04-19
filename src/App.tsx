525018826371e8ca6083d8be696ab8110a03c509
PolaroidZone <tsogangamosweswe@gmail.com>
2025-04-19 20:34:56 +0200

    Fixed login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { useState, useEffect } from "react";
 import Index from "@/pages/Index";
 import Profile from "@/pages/Profile";
@@ -23,6 +27,7 @@ import "./App.css";
 import { RentalReview } from "./pages/RentalReview";
 import RentalDetailsRefactored from "./pages/RentalDetailsRefactored";
 import Signup from "./pages/signup";
+import Login from "./pages/Login";
 import { Toaster as ShadcnToaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "sonner";
 import ResetPassword from "@/pages/ResetPassword";
@@ -30,7 +35,7 @@ import ResetPassword from "@/pages/ResetPassword";
 const PageTransitionLoader = () => {
   const location = useLocation();
   const [isLoading, setIsLoading] = useState(false);
-  
+
   useEffect(() => {
     setIsLoading(true);
     const timer = setTimeout(() => {
@@ -38,9 +43,9 @@ const PageTransitionLoader = () => {
     }, 800);
     return () => clearTimeout(timer);
   }, [location.pathname]);
-  
+
   if (!isLoading) return null;
-  
+
   return (
     <div className="fixed top-0 left-0 w-full z-50 bg-background/80">
       <div className="flex flex-col items-center justify-center py-3">
@@ -56,18 +61,96 @@ const AppRoutes = () => {
       <PageTransitionLoader />
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/map" element={<ProtectedRoute><Map /></ProtectedRoute>} />
-        <Route path="/more" element={<ProtectedRoute><More /></ProtectedRoute>} />
-        <Route path="/cars/:id" element={<ProtectedRoute><CarDetails /></ProtectedRoute>} />
-        <Route path="/add-car" element={<ProtectedRoute><AddCar /></ProtectedRoute>} />
-        <Route path="/driver-license" element={<ProtectedRoute><DriverLicense /></ProtectedRoute>} />
-        <Route path="/edit-car/:id" element={<ProtectedRoute><EditCar /></ProtectedRoute>} />
-        <Route path="/bookings" element={<ProtectedRoute><Bookings /></ProtectedRoute>} />
-        <Route path="/notifications" element={<ProtectedRoute><Notifications /></ProtectedRoute>} />
-        <Route path="/saved-cars" element={<ProtectedRoute><SavedCars /></ProtectedRoute>} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/map"
+          element={
+            <ProtectedRoute>
+              <Map />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/more"
+          element={
+            <ProtectedRoute>
+              <More />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cars/:id"
+          element={
+            <ProtectedRoute>
+              <CarDetails />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-car"
+          element={
+            <ProtectedRoute>
+              <AddCar />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/driver-license"
+          element={
+            <ProtectedRoute>
+              <DriverLicense />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-car/:id"
+          element={
+            <ProtectedRoute>
+              <EditCar />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/bookings"
+          element={
+            <ProtectedRoute>
+              <Bookings />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/notifications"
+          element={
+            <ProtectedRoute>
+              <Notifications />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/saved-cars"
+          element={
+            <ProtectedRoute>
+              <SavedCars />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/notifications/:id"
           element={
@@ -85,7 +168,14 @@ const AppRoutes = () => {
           }
         />
         <Route path="/rental-review/:bookingId" element={<RentalReview />} />
-        <Route path="/rental-details/:id" element={<ProtectedRoute><RentalDetailsRefactored /></ProtectedRoute>} />
+        <Route
+          path="/rental-details/:id"
+          element={
+            <ProtectedRoute>
+              <RentalDetailsRefactored />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </>
