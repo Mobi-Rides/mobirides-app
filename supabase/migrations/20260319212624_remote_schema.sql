22df934ac987deb731b10eb3f68855a7b088905c
maphanyane <maphanyane@mobirides.com>
2026-03-27 12:36:24 +0200

    feat: Create migration to add the remote Supabase schema.

--- supabase/migrations/20260319212624_remote_schema.sql
+++ supabase/migrations/20260319212624_remote_schema.sql
@@ -1020,11 +1020,13 @@ alter table "public"."bookings" alter column "created_at" set default timezone('
 
 alter table "public"."bookings" alter column "created_at" set not null;
 
+alter table "public"."bookings" alter column "status" drop default;
+
+alter table "public"."bookings" alter column "status" set data type public.booking_status using "status"::text::public.booking_status;
+
 alter table "public"."bookings" alter column "status" set default 'pending'::public.booking_status;
 
 alter table "public"."bookings" alter column "status" set not null;
-
-alter table "public"."bookings" alter column "status" set data type public.booking_status using "status"::text::public.booking_status;
 
 alter table "public"."bookings" alter column "total_price" set data type numeric using "total_price"::numeric;
 
@@ -1132,9 +1134,11 @@ alter table "public"."notifications" alter column "id" add generated always as i
 
 alter table "public"."notifications" alter column "metadata" drop default;
 
-alter table "public"."notifications" alter column "role_target" set default 'system_wide'::public.notification_role;
+alter table "public"."notifications" alter column "role_target" drop default;
 
 alter table "public"."notifications" alter column "role_target" set data type public.notification_role using "role_target"::text::public.notification_role;
+
+alter table "public"."notifications" alter column "role_target" set default 'system_wide'::public.notification_role;
 
 alter table "public"."notifications" alter column "title" set not null;
 
@@ -1166,9 +1170,11 @@ alter table "public"."profiles" alter column "created_at" set default timezone('
 
 alter table "public"."profiles" alter column "created_at" set not null;
 
-alter table "public"."profiles" alter column "role" set default 'renter'::public.user_role;
+alter table "public"."profiles" alter column "role" drop default;
 
 alter table "public"."profiles" alter column "role" set data type public.user_role using "role"::text::public.user_role;
+
+alter table "public"."profiles" alter column "role" set default 'renter'::public.user_role;
 
 alter table "public"."profiles" alter column "updated_at" set default timezone('utc'::text, now());
 
@@ -3763,6 +3769,7 @@ END;
 $function$
 ;
 
+DROP FUNCTION IF EXISTS public.cleanup_expired_notifications();
 CREATE OR REPLACE FUNCTION public.cleanup_expired_notifications()
  RETURNS trigger
  LANGUAGE plpgsql
